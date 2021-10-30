524a56bde720dbb81398243cf4a7aebd26e381b2
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2021-10-30 18:30:02 +0200

    Init commit

---
+++ arduino_relayboard.ino
@@ -0,0 +1,17 @@
+/// @brief This method adds two integers.
+/// @param a First integer to add.
+/// @param b Second integer to add.
+/// @return The sum of both parameters.
+
+
+#include "arduino_class.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
